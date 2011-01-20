6faa634c59737ed48cceb98c4dc2905c316e4752
Erico Andrei <erico@simplesconsultoria.com.br>
2011-01-20 20:43:21 +0000

    Adds a new browser layer to social bookmarks

--- sc/social/bookmarks/browser/interfaces.py
+++ sc/social/bookmarks/browser/interfaces.py
@@ -1,5 +1,5 @@
-from plone.theme.interfaces import IDefaultPloneLayer
+from zope.interface import Interface
 
-class IThemeSpecific(IDefaultPloneLayer):
-    """Marker interface that defines a Zope 3 browser layer.
+class ISocialBookmarksLayer(Interface):
+    """ Marker interface that defines a Zope 3 browser layer
     """
