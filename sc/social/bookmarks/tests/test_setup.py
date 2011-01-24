5a395582046e7c3cd5c1342fe9ba8a33611f5151
Johannes Raggam <raggam-nl@adm.at>
2011-01-24 13:21:40 +0000

    remove kss

--- sc/social/bookmarks/tests/test_setup.py
+++ sc/social/bookmarks/tests/test_setup.py
@@ -7,16 +7,11 @@ from sc.social.bookmarks.config import PROJECTNAME
 from sc.social.bookmarks.tests.layer import SocialBookmarksLayer
 
 STYLESHEETS = (
-    '++resource++sb_stylesheets/social_bookmark.css',
+    '++resource++sb_resources/social_bookmark.css',
     )
 
 JAVASCRIPTS = (
-    'sarissa.js',
-    '++resource++kukit.js',
-    )
-
-KSS = (
-    '++resource++sb_stylesheets/share.kss',
+    '++resource++sb_resources/social_bookmarks_overlay.js',
     )
 
 CONFIGLETS = (
@@ -34,10 +29,6 @@ class InstallTest(PloneTestCase):
     def test_javascripts(self):
         for js in JAVASCRIPTS:
             self.failUnless(js in self.portal.portal_javascripts.getResourceIds(), '%s javascript not installed' % js)
-
-    def test_kss(self):
-        for k in KSS:
-            self.failUnless(k in self.portal.portal_kss.getResourceIds(), '%s kss not installed' % k)
 
     def test_configlets(self):
         installed = [a.getAction(self)['id'] for a in self.portal.portal_controlpanel.listActions()]
@@ -68,10 +59,6 @@ class UninstallTest(PloneTestCase):
     def test_javascripts(self):
         for js in JAVASCRIPTS:
             self.failIf(js in self.portal.portal_javascripts.getResourceIds(), '%s javascript not installed' % js)
-
-    def test_kss(self):
-        for k in KSS:
-            self.failIf(k in self.portal.portal_kss.getResourceIds(), '%s kss not uninstalled' % k)
 
     def test_configlets(self):
         installed = [a.getAction(self)['id'] for a in self.portal.portal_controlpanel.listActions()]
