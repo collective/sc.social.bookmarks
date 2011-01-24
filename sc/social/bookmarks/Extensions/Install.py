3e0473962537c480504a16640fddc4806fa7a915
Johannes Raggam <raggam-nl@adm.at>
2011-01-24 13:47:28 +0000

    plone2 install extensions not needed anymore

--- sc/social/bookmarks/Extensions/Install.py
+++
@@ -1,7 +0,0 @@
-from Products.CMFCore.utils import getToolByName
-
-def uninstall(portal):
-    setup_tool = getToolByName(portal, 'portal_setup')
-    setup_tool.runAllImportStepsFromProfile('profile-sc.social.bookmarks:uninstall')
-    setup_tool.setBaselineContext('profile-Products.CMFPlone:plone')
-    return "Ran all uninstall steps."
