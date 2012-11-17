ed6d407c5c21806d0b0e1a92447676d1c9738547
Érico Andrei <erico@simplesconsultoria.com.br>
2012-11-17 12:53:07 -0200

    PEP8

--- src/sc/social/bookmarks/vocabularies.py
+++ src/sc/social/bookmarks/vocabularies.py
@@ -9,6 +9,7 @@ from zope.schema.vocabulary import SimpleVocabulary
 from zope.schema.vocabulary import SimpleTerm
 from sc.social.bookmarks.config import all_providers
 
+
 class SocialBookmarksProvidersVocabulary(object):
     """
     """
@@ -16,8 +17,8 @@ class SocialBookmarksProvidersVocabulary(object):
 
     def __call__(self, context):
         context = getattr(context, 'context', context)
-        items = [ SimpleTerm(p.get('id'), p.get('id'), p.get('id'))
-                  for p in all_providers]
+        items = [SimpleTerm(p.get('id'), p.get('id'), p.get('id'))
+                 for p in all_providers]
         return SimpleVocabulary(items)
 
 SocialBookmarksProvidersVocabularyFactory = SocialBookmarksProvidersVocabulary()
