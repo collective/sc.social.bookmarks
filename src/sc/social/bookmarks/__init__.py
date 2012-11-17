564c98f7105803880ff77d43670dd0f5f31370dc
Érico Andrei <erico@simplesconsultoria.com.br>
2012-11-17 12:52:52 -0200

    PEP8

--- src/sc/social/bookmarks/__init__.py
+++ src/sc/social/bookmarks/__init__.py
@@ -1,5 +1,8 @@
+# -*- coding: utf-8 -*-
+
 from zope.i18nmessageid import MessageFactory
 _ = MessageFactory('sc.social.bookmarks')
+
 
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
