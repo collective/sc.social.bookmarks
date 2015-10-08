8465240aa7dad26b15310660ed967d0bf4ff3e03
Ben Cole <benc@netsight.co.uk>
2015-10-08 14:05:22 +0100

    Preparing release 1.3.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.3.2.dev0'
+version = '1.3.2'
 long_description = (open("README.txt").read() + "\n" +
                     open(os.path.join("docs", "INSTALL.txt")).read() + "\n" +
                     open(os.path.join("docs", "CREDITS.txt")).read() + "\n" +
