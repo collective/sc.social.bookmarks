fb7fcfc5dd22e2e484bbc6ede486c101323b3b27
wesleybl <wesleybl@gmail.com>
2021-12-13 11:39:44 -0300

    Remove old upgrade steps.

--- CHANGES.rst
+++ CHANGES.rst
@@ -4,6 +4,9 @@ Changelog
 1.3dev (unreleased)
 -------------------
 
+- Remove old upgrade steps.
+  [wesleybl]
+
 - Add support to Python 3.6, 3.7 and 3.8.
   [wesleybl]
 
@@ -20,7 +23,6 @@ Changelog
   [ericof]
 
 - Use plone.registry instead portal_properties.
-  A migration step is included to migrate old settings to the registry.
   [thet]
 
 - Add bookmark titles to the <a> element instead of the <img>.
--- src/sc/social/bookmarks/tests/test_setup.py
+++ src/sc/social/bookmarks/tests/test_setup.py
@@ -4,7 +4,6 @@ from plone.app.testing import TEST_USER_ID
 from plone.browserlayer.utils import registered_layers
 from plone.registry.interfaces import IRegistry
 from sc.social.bookmarks.config import PROJECTNAME
-from sc.social.bookmarks.controlpanel.bookmarks import IProvidersSchema
 from sc.social.bookmarks.testing import INTEGRATION_TESTING
 from zope.component import getUtility
 
@@ -26,25 +25,6 @@ class InstallTestCase(unittest.TestCase):
         self.portal = self.layer["portal"]
         self.props = self.portal["portal_properties"]
         self.registry = getUtility(IRegistry)
-
-    def _load_profile(self, st=None, profile_id=""):
-        if not (st and profile_id):
-            return
-        from Products.CMFCore.interfaces import ISiteRoot
-        from Products.GenericSetup import EXTENSION
-        from Products.GenericSetup import profile_registry
-
-        profile_registry.registerProfile(
-            name=profile_id,
-            title=profile_id,
-            description=(profile_id),
-            path="profiles/{0}".format(profile_id),
-            product="sc.social.bookmarks.tests",
-            profile_type=EXTENSION,
-            for_=ISiteRoot,
-        )
-        profile = "profile-sc.social.bookmarks.tests:{0}".format(profile_id)
-        st.runAllImportStepsFromProfile(profile)
 
     @unittest.skip("Test failure in Plone 5.2")
     def test_installed(self):
@@ -72,61 +52,6 @@ class InstallTestCase(unittest.TestCase):
             self.assertTrue(
                 resource_id in resource_ids, "{0} not installed".format(resource_id)
             )
-
-    def test_upgrade_1_to_2(self):
-        profile_id = "sc.social.bookmarks:default"
-        st = getattr(self.portal, "portal_setup")
-        st.setLastVersionForProfile(profile_id, u"1")
-        upgrades = st.listUpgrades(profile_id)
-        step_info = upgrades[0]
-        self.assertEqual(step_info.get("ssource"), "1")
-        self.assertEqual(step_info.get("sdest"), "2")
-        step = step_info["step"]
-        step.doStep(st)
-        new_version = st.getLastVersionForProfile(profile_id)
-        self.assertEqual(new_version, (u"2",))
-
-    def test_upgrade_2_to_3(self):
-        st = getattr(self.portal, "portal_setup")
-        profile_id = "sc.social.bookmarks:default"
-        st.setLastVersionForProfile(profile_id, u"2")
-        upgrades = st.listUpgrades(profile_id)
-        step_info = upgrades[0]
-        self.assertEqual(step_info.get("ssource"), "2")
-        self.assertEqual(step_info.get("sdest"), "3")
-        step = step_info["step"]
-        step.doStep(st)
-        new_version = st.getLastVersionForProfile(profile_id)
-        self.assertEqual(new_version, (u"3",))
-
-    def test_upgrade_2_to_3_property_sheet_migration(self):
-        # Load a profile that will create a property sheet
-        # as we used to have until version 3
-        st = getattr(self.portal, "portal_setup")
-        self._load_profile(st, "version2")
-
-        props = self.portal.portal_properties
-        sheet = getattr(props, "sc_social_bookmarks_properties", None)
-        self.assertFalse(sheet is None)
-        self.assertEqual(sheet.bookmark_providers, ("Reddit", "Twitter"))
-        self.assertEqual(sheet.enabled_portal_types, ("Document",))
-
-        profile_id = "sc.social.bookmarks:default"
-        st.setLastVersionForProfile(profile_id, u"2")
-        upgrades = st.listUpgrades(profile_id)
-        step = upgrades[0]["step"]
-        step.doStep(st)
-
-        sheet = getattr(props, "sc_social_bookmarks_properties", None)
-        self.assertTrue(sheet is None)
-        settings = self.registry.forInterface(
-            IProvidersSchema, prefix="sc.social.bookmarks."
-        )
-        self.assertTrue(hasattr(settings, "bookmark_providers"))
-        self.assertEqual(settings.bookmark_providers, ("Reddit", "Twitter"))
-
-        self.assertTrue(hasattr(settings, "enabled_portal_types"))
-        self.assertEqual(settings.enabled_portal_types, ("Document",))
 
 
 class UninstallTestCase(unittest.TestCase):
